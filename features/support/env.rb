60e09cf7f9eca487e40074e87dcaeeab9fe847c7
SoutoThyago <thyagocsouto@gmail.com>
2024-10-15 20:34:50 -0300

    change to new environment file

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,13 +1,13 @@
 #Variaveis globais
-AMBIENTE = ENV['AMBIENTE']
-SO = ENV['SO']
-BROWSER = ENV['BROWSER']
-REMOTE_DRIVER = ENV['REMOTE_DRIVER']
-HUB_URL = ENV['HUB_URL']
-TIMEOUT_PADRAO = ENV['TIMEOUT_PADRAO']
-SALVAR_SCREENSHOT_PARA_CADA_PASSO = ENV['SALVAR_SCREENSHOT_PARA_CADA_PASSO']
-DIRETORIO_DOWNLOADS = ENV['DIRETORIO_DOWNLOADS']
-CONFIG = YAML.load_file(File.dirname(__FILE__)+"/ambientes/#{AMBIENTE}.yml") 
+SAVE_SCREENSHOT_STEP_BY_STEP = ENV['SAVE_SCREENSHOT_STEP_BY_STEP']
+DIRETORIO_DOWNLOADS          = ENV['DIRETORIO_DOWNLOADS']
+TIMEOUT_PADRAO               = ENV['TIMEOUT_PADRAO']
+REMOTE_DRIVER                = ENV['REMOTE_DRIVER']
+AMBIENTE                     = ENV['AMBIENTE']
+BROWSER                      = ENV['BROWSER']
+HUB_URL                      = ENV['HUB_URL']
+CONFIG                       = YAML.load_file(File.dirname(__FILE__)+"/environments/#{AMBIENTE}.yml")
+SO                           = ENV['SO']
 
 require 'capybara/cucumber'
 require 'selenium-webdriver'
@@ -21,10 +21,15 @@ require 'roo-xls'
 require_relative 'driver_helper.rb'
 require_relative '../helpers/page_helper.rb'
 
-
 World(PageObjects)
 AllureCucumber.configure do |config|
     config.results_directory = "reports_allure"
     config.clean_results_directory = true
     config.logging_level = Logger::INFO
 end
+
+#personal variables
+EMAILPROD       = 'raiinmaker.qa@gmail.com'
+PASSWORDPROD    = 'RaiinCoin$2024'  
+EMAILSTAGING    = 'thyago@raiinmaker.com'
+PASSWORDSTAGING = '1955$outO'
