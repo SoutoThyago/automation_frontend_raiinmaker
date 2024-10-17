a8ae1d0a74f96d8d641746bde976f9949b91b052
SoutoThyago <thyagocsouto@gmail.com>
2024-10-16 22:06:41 -0300

    add balance Object

--- features/helpers/page_helper.rb
+++ features/helpers/page_helper.rb
@@ -3,9 +3,14 @@ Dir[File.join(File.dirname(__FILE__), "../../lib/pages/*/*_page.rb")].each { |fi
 module PageObjects   
 
   require_relative '../../lib/pages/customer/loginPage'
+  require_relative '../../lib/pages/balance/balancePage'
 
   def loginPage
     @loginPage ||= LoginPage.new
-  end  
+  end
+
+  def balancePage
+    @balancePage = BalancePage.new
+  end
 
 end
